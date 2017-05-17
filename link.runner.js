eaa4b748b1c2f17c16b2007fe21eabfe9f24c2b6
Wayne <why__t@hotmail.com>
2017-05-16 21:13:13 -0600

    hopefully adds some sweet link functionality

--- link.runner.js
+++ link.runner.js
@@ -1,12 +1,16 @@
-var sHelper = require('structure.helper')
-
 module.exports = {
-  run: function() {
-
-
+  StructureLink.prototype.run = function() {
+    const flagOnLink = this.room.lookForAt(LOOK_FLAGS, this.pos)
+    if(flagOnLink && flagOnLink.name == 'from') {
+      const allFlags = this.room.lookFor(LOOK_FLAGS)
+      for(var flag in allFlags) {
+        if(flag.name == 'to') {
+          const linkUnderToFlag = flag.room.lookForAt(LOOK_STRUCTURES, flag.pos))
+          if(linkUnderToFlag && linkUnderToFlag.energy < linkUnderToFlag.energyCapacity) {
+            this.transferEnergy(linkUnderToFlag)
+          }
+        }
+      }
+    }
   }
-}
-
-var _initLinkMemory = function() {
-  if(!Memory.links) Memory.links = {}
 }
